d55e142e1ee6f5b6b3a72c07e6c35758b4e3be12
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-01 12:10:08 -0300

    init header card component

---
+++ src/components/HeaderCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <!-- <img :src="j" alt="image">
+        <h2> {{ headerText }} </h2>
+        <p> {{ paragText }} </p> -->
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: ["img", "headerText", "paragText"],
+    setup(prop) {
+        console.log(prop.headerText, prop.paragText)
+    }
+})
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
