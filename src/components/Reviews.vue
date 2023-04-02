15ed792092c1031949767a50b9bef915270b186e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-02 01:47:32 -0300

    Init reviews card

---
+++ src/components/Reviews.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img src="../assets/Review.png" alt="reviews">
+        <p>
+            Certainty say suffering his him collected intention promotion. Hill sold ham men made lose case. Views abode law heard jokes too.
+        </p>
+        
+        <div>
+            <p>Andrew Chris</p>
+            <p>Client from Uganda</p>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+
+</script>
+
+
+<style lang="scss">
+
+</style>
