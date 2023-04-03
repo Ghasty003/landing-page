121fa523b43fcb90dfbe2cbcb957e9d99296d9de
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-03 00:17:11 -0300

    updated

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="review_card">
         <img src="../assets/Review.png" alt="reviews">
         <p>
             Certainty say suffering his him collected intention promotion. Hill sold ham men made lose case. Views abode law heard jokes too.
@@ -19,5 +19,21 @@
 
 
 <style lang="scss">
+
+.review_card {
+    width: 450px;
+    height: 250px;
+    background: #FFFFFF;
+    box-shadow: 0px 41.04px 42.48px rgba(0, 0, 0, 0.053);
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    color: #3E4581;
+    text-align-last: center;
+    padding: 30px 33px;
+}
 
 </style>
